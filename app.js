5f451ce6f1beb729c0f6bb5936f307a5cfbb1104
penixab13 <115022449+penixab13@users.noreply.github.com>
2024-05-16 11:11:20 +0100

    add other changes for test

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hasnae is my name'));
+app.get('/', (req, res) => res.send('Hasnae is my name and my last name is Abouchouar'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
